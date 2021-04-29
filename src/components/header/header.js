7c92f6225b2bef62630c577f6208df01800064e1
Trent <mr.peschke@gmail.com>
2021-04-29 14:41:50 -0600

    upgrading the stylings

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,7 @@ export default function Header(Page) {
     return props =>
         <div>
             <div className="headerShell">
-                <Link to="/">Bonfire Character Vault (beta)</Link>
+                <Link to="/"><h1>Bonfire Character Vault</h1><p className="beta">(beta)</p></Link>
                 <Link to="/"><i className="fas fa-home"></i></Link>
             </div>
             <Page {...props} />
--- src/components/home/model.js
+++ src/components/home/model.js
@@ -15,7 +15,7 @@ export default class Modal extends Component {
         this.setState({ loading: true }, _ => {
             axios.patch(`/api/removeCharacter/${this.props.characterId}`).then(data => {
                 this.props.toggleModel(false)
-                this.setState({loading: false})
+                this.setState({ loading: false })
             })
         })
     }
@@ -31,12 +31,28 @@ export default class Modal extends Component {
         return (
             <div className={showModel ? "overlay" : "displayNone"}>
                 <div className="modelBody">
-                    <h5>Sure You Want to Remove This Character?</h5>
-                    <p>You'll still be able to access it through the Vault.</p>
-                    <p>However, it'll be a pain to find and you won't be able to edit it.</p>
-                    <div className="modelButtons">
-                        <button className="yes" onClick={this.removeCharacter}>Go For It</button>
-                        <button className="no" onClick={_ => toggleModel(false)}>Wait, Nevermind</button>
+                    <h5><i class="fas fa-exclamation-triangle"></i>Sure You Want to Remove This Character?</h5>
+                    <div className="modelInnards">
+                        <p>You'll still be able to access it through the Vault if you've filled out the following:</p>
+                        <ul>
+                            <li>Race</li>
+                            <li>Archetypes</li>
+                            <li>Level</li>
+                            <li>Primary Drawback</li>
+                            <li>Stats</li>
+                            <li>Honor</li>
+                            <li>Temperament</li>
+                            <li>Devotions</li>
+                            <li>Flaws</li>
+                            <li>Traits</li>
+                            <li>Archetype Ability</li>
+                            <li>Gear</li>
+                        </ul>
+                        <p>However, it'll be a pain to find and you won't be able to edit it.</p>
+                        <div className="modelButtons">
+                            <button className="yes" onClick={this.removeCharacter}>Go For It</button>
+                            <button className="no" onClick={_ => toggleModel(false)}>Wait, Nevermind</button>
+                        </div>
                     </div>
                 </div>
             </div>
