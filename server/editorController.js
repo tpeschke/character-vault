f771f7ad57e681d592a425a239a3f54b5a86c74e
Trent <mr.peschke@gmail.com>
2021-09-16 16:17:15 -0600

    bug squashing

--- server/editorController.js
+++ server/editorController.js
@@ -159,77 +159,77 @@ editController = {
                 return goals.map(({ id: goalid, value }) => {
                     return db.upsert.goals(goalid, id, value)
                 })
-            }))
+            }).catch(e => console.log("line 162 ~ ", e)))
             promiseArray.push(db.delete.devotions([id, [0, ...devotions.map(devotions => devotions.id)]]).then(_ => {
                 return devotions.map(({ id: devotionid, value, title }) => {
                     return db.upsert.devotions(devotionid, id, title, setToMin(value, 0))
                 })
-            }))
+            }).catch(e => console.log("line 167 ~ ", e)))
             promiseArray.push(db.delete.flaws([id, [0, ...flaws.map(flaws => flaws.id)]]).then(_ => {
                 return flaws.map(({ id: flawid, value, title }) => {
                     return db.upsert.flaws(flawid, id, title, value)
                 })
-            }))
+            }).catch(e => console.log("line 172 ~ ", e)))
             promiseArray.push(db.delete.traits([id, [0, ...req.body.traits.map(traits => traits.id)]]).then(_ => {
                 return req.body.traits.map(({ id: traitsid, value, title }) => {
                     return db.upsert.traits(traitsid, id, title, value)
                 })
-            }))
+            }).catch(e => console.log("line 177 ~ ", e)))
             promiseArray.push(db.delete.reputation([id, [0, ...reputation.map(reputation => reputation.id)]]).then(_ => {
                 return reputation.map(({ id: reputationid, value }) => {
                     return db.upsert.reputation(reputationid, id, value)
                 })
-            }))
+            }).catch(e => console.log("line 182 ~ ", e)))
             promiseArray.push(db.delete.gearone([id, [0, ...gearone.map(gearone => gearone.id)]]).then(_ => {
                 return gearone.map(({ id: gearoneid, value, title }) => {
                     return db.upsert.gearone(gearoneid, id, title, value)
                 })
-            }))
+            }).catch(e => console.log("line 187 ~ ", e)))
             promiseArray.push(db.delete.geartwo([id, [0, ...geartwo.map(geartwo => geartwo.id)]]).then(_ => {
                 return geartwo.map(({ id: geartwoid, value, title }) => {
                     return db.upsert.geartwo(geartwoid, id, title, value)
                 })
-            }))
+            }).catch(e => console.log("line 192 ~ ", e)))
             promiseArray.push(db.delete.gearthree([id, [0, ...gearthree.map(gearthree => gearthree.id)]]).then(_ => {
                 return gearthree.map(({ id: gearthreeid, value, title }) => {
                     return db.upsert.gearthree(gearthreeid, id, title, value)
                 })
-            }))
+            }).catch(e => console.log("line 197 ~ ", e)))
             promiseArray.push(db.delete.gearfour([id, [0, ...gearfour.map(gearfour => gearfour.id)]]).then(_ => {
                 return gearfour.map(({ id: gearfourid, value, title }) => {
                     return db.upsert.gearfour(gearfourid, id, title, value)
                 })
-            }))
+            }).catch(e => console.log("line 202 ~ ", e)))
             let { weaponid, trainattack, trainparry, trainrecovery, traindamage, miscattack, miscparry, miscrecovery, miscdamage, miscinit, name, basedamage, baserecovery, baseparry, basemeasure, type, bonus, traits, size } = weaponone
-            promiseArray.push(db.upsert.weaponone(weaponid, id, trainattack, trainparry, trainrecovery, traindamage, miscattack, miscparry, setToMin(miscrecovery, 0), miscdamage, miscinit, name, basedamage, baserecovery, baseparry, basemeasure, type, bonus, traits, size))
+            promiseArray.push(db.upsert.weaponone(weaponid, id, trainattack, trainparry, trainrecovery, traindamage, miscattack, miscparry, setToMin(miscrecovery, 0), miscdamage, miscinit, name, basedamage, baserecovery, baseparry, basemeasure, type, bonus, traits, size).catch(e => console.log("line 204 ~ ", e)))
 
             let { weaponid: weaponidtwo, trainattack: trainattacktwo, trainparry: trainparrytwo, trainrecovery: trainrecoverytwo, traindamage: traindamagetwo, miscattack: miscattacktwo, miscparry: miscparrytwo, miscrecovery: miscrecoverytwo, miscdamage: miscdamagetwo, miscinit: miscinittwo, name: nametwo, basedamage: basedamagetwo, baserecovery: baserecoverytwo, baseparry: baseparrytwo, basemeasure: basemeasuretwo, type: typetwo, bonus: bonustwo, traits: traitstwo, size: sizetwo } = weapontwo
-            promiseArray.push(db.upsert.weapontwo(weaponidtwo, id, trainattacktwo, trainparrytwo, trainrecoverytwo, traindamagetwo, miscattacktwo, miscparrytwo, setToMin(miscrecoverytwo, 0), miscdamagetwo, miscinittwo, nametwo, basedamagetwo, baserecoverytwo, baseparrytwo, basemeasuretwo, typetwo, bonustwo, traitstwo, sizetwo))
+            promiseArray.push(db.upsert.weapontwo(weaponidtwo, id, trainattacktwo, trainparrytwo, trainrecoverytwo, traindamagetwo, miscattacktwo, miscparrytwo, setToMin(miscrecoverytwo, 0), miscdamagetwo, miscinittwo, nametwo, basedamagetwo, baserecoverytwo, baseparrytwo, basemeasuretwo, typetwo, bonustwo, traitstwo, sizetwo).catch(e => console.log("line 207 ~ ", e)))
 
             let { weaponid: weaponidthree, trainattack: trainattackthree, trainparry: trainparrythree, trainrecovery: trainrecoverythree, traindamage: traindamagethree, miscattack: miscattackthree, miscparry: miscparrythree, miscrecovery: miscrecoverythree, miscdamage: miscdamagethree, miscinit: miscinitthree, name: namethree, basedamage: basedamagethree, baserecovery: baserecoverythree, baseparry: baseparrythree, basemeasure: basemeasurethree, type: typethree, bonus: bonusthree, traits: traitsthree, size: sizethree } = weaponthree
-            promiseArray.push(db.upsert.weaponthree(weaponidthree, id, trainattackthree, trainparrythree, trainrecoverythree, traindamagethree, miscattackthree, miscparrythree, setToMin(miscrecoverythree, 0), miscdamagethree, miscinitthree, namethree, basedamagethree, baserecoverythree, baseparrythree, basemeasurethree, typethree, bonusthree, traitsthree, sizethree))
+            promiseArray.push(db.upsert.weaponthree(weaponidthree, id, trainattackthree, trainparrythree, trainrecoverythree, traindamagethree, miscattackthree, miscparrythree, setToMin(miscrecoverythree, 0), miscdamagethree, miscinitthree, namethree, basedamagethree, baserecoverythree, baseparrythree, basemeasurethree, typethree, bonusthree, traitsthree, sizethree).catch(e => console.log("line 210 ~ ", e)))
 
             let { weaponid: weaponidfour, trainattack: trainattackfour, trainrecovery: trainrecoveryfour, traindamage: traindamagefour, miscattack: miscattackfour, miscrecovery: miscrecoveryfour, miscdamage: miscdamagefour, miscinit: miscinitfour, name: namefour, basedamage: basedamagefour, baserecovery: baserecoveryfour, type: typefour, bonus: bonusfour, traits: traitsfour, size: sizefour } = weaponfour
-            promiseArray.push(db.upsert.weaponfour(weaponidfour, id, trainattackfour, trainrecoveryfour, traindamagefour, miscattackfour, setToMin(miscrecoveryfour, 0), miscdamagefour, miscinitfour, namefour, basedamagefour, baserecoveryfour, typefour, bonusfour, traitsfour, sizefour))
+            promiseArray.push(db.upsert.weaponfour(weaponidfour, id, trainattackfour, trainrecoveryfour, traindamagefour, miscattackfour, setToMin(miscrecoveryfour, 0), miscdamagefour, miscinitfour, namefour, basedamagefour, baserecoveryfour, typefour, bonusfour, traitsfour, sizefour).catch(e => console.log("line 213 ~ ", e)))
 
             promiseArray.push(db.upsert.armor(armorid, id, armorname, armordr, armorskilladj, armorbonus, armortrainingdef, armortrainrecovery, +armortrainfatigue, armortraininit, armormiscdef, armormiscrecovery, armormiscinit, +armormiscfatigue, armorbasedef, armorbaserecovery, +armorbasefatigue, armorbaseinit))
-            promiseArray.push(db.upsert.shield(shieldid, id,shieldname, shielddr, shieldsize, shieldcover, shieldbonus, shieldbasedef, shieldbaseparry, +shieldbasefatigue, shieldbasebreak, shieldtraindef, shieldtrainparry, +shieldtrainfatigue, shieldtrainbreak, shieldmiscdef, shieldmiscparry, shieldmiscbreak, +shieldmiscfatigue))
+            promiseArray.push(db.upsert.shield(shieldid, id, shieldname, shielddr, shieldsize, shieldcover, shieldbonus, shieldbasedef, shieldbaseparry, shieldbasefatigue, shieldbasebreak, shieldtraindef, shieldtrainparry, shieldtrainfatigue, shieldtrainbreak, shieldmiscdef, shieldmiscparry, shieldmiscbreak, shieldmiscfatigue).catch(e => console.log("line 216 ~ ", e)))
 
             let { nativeid, language, rank: languagerank } = nativelanguage
-            promiseArray.push(db.upsert.nativeLanguage(nativeid, id, language, languagerank))
+            promiseArray.push(db.upsert.nativeLanguage(nativeid, id, language, languagerank).catch(e => console.log("line 219 ~ ", e)))
 
             skillsuites.forEach(({ skillsuiteid, rank, characterskillsuitesid }) => {
                 if (characterskillsuitesid) {
-                    promiseArray.push(db.upsert.insert.skillsuites(rank, characterskillsuitesid))
+                    promiseArray.push(db.upsert.insert.skillsuites(rank, characterskillsuitesid).catch(e => console.log("line 223 ~ ", e)))
                 } else if (!characterskillsuitesid) {
-                    promiseArray.push(db.upsert.add.skillsuites(skillsuiteid, id, rank))
+                    promiseArray.push(db.upsert.add.skillsuites(skillsuiteid, id, rank).catch(e => console.log("line 225 ~ ", e)))
                 }
             })
             promiseArray.push(db.delete.skills([id, [0, ...skills.map(skills => skills.id)]]).then(_ => {
                 return skills.map(({ id: skillsid, cost, skill, rank, mod }) => {
-                    return db.upsert.skills(skillsid, id, skill, cost, rank, mod)
+                    return db.upsert.skills(skillsid, id, skill, cost, rank, mod).catch(e => console.log("line 230 ~ ", e))
                 })
-            }))
+            }).catch(e => console.log("line 232 ~ ", e)))
 
             Promise.all(promiseArray).then(_ => {
                 assembleCharacter(req).then((character) => {
