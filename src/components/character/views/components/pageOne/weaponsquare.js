7a6c103e830a7f6dfad5c22ec2858c9e4f5cf03d
Trent <mr.peschke@gmail.com>
2022-09-30 14:19:55 -0600

    bug squashing

--- src/components/character/views/components/pageOne/weaponsquare.js
+++ src/components/character/views/components/pageOne/weaponsquare.js
@@ -158,7 +158,7 @@ function displayDamage(basedamage, damageType, traingingDamage, strDam) {
                 diceObject.d3s += 1
             } else if (leftover === 2) {
                 diceObject.d4s += 1
-            } else if (leftover === 4) {
+            } else if (leftover === 3) {
                 diceObject.d6s += 1
             }
         } else {
