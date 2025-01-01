26eb48bd0204c8006a924777ec23b679dbec2800
Trent <mr.peschke@gmail.com>
2025-01-01 16:18:15 -0700

    ranges edit css overhaul

--- src/components/character/views/components/pageOne/ranges/ranges.js
+++ src/components/character/views/components/pageOne/ranges/ranges.js
@@ -2,47 +2,23 @@ import React from 'react'
 import './ranges.css'
 
 export default function Ranges({ maxrange, editing, updateAttribute }) {
-
-    if (editing) {
+    function calculateRange(rangeIncrement, min, max, isMax) {
+        if (editing && isMax) {
+            return (
+                <div>
+                    <p>{rangeIncrement}</p>
+                    <p className="maxrangeLocation">{min}</p>
+                    <p>-</p>
+                    <input className="maxrangeLocation" type="number" defaultValue={max} onChange={event => updateAttribute(event.target.value, "maxrange")} />
+                </div>
+            )
+        }
         return (
-            <div className="rangeShell">
-                <h2>Ranges (ft)</h2>
-                <div>
-                    <p>0</p>
-                    <p className="maxrangeLocation one">5</p>
-                    <p>-</p>
-                    <p className="maxrangeLocation two">{(maxrange / 6).toFixed(0)}</p>
-                </div>
-                <div>
-                    <p>-2</p>
-                    <p className="maxrangeLocation three">{+(maxrange / 6).toFixed(0) + 1}</p>
-                    <p>-</p>
-                    <p className="maxrangeLocation four">{(maxrange / 3).toFixed(0)}</p>
-                </div>
-                <div>
-                    <p>-4</p>
-                    <p className="maxrangeLocation five">{+(maxrange / 3).toFixed(0) + 1}</p>
-                    <p>-</p>
-                    <p className="maxrangeLocation six">{(maxrange / 2).toFixed(0)}</p>
-                </div>
-                <div>
-                    <p>-8</p>
-                    <p className="maxrangeLocation seven">{+(maxrange / 2).toFixed(0) + 1}</p>
-                    <p>-</p>
-                    <p className="maxrangeLocation eight">{(maxrange / 1.5).toFixed(0)}</p>
-                </div>
-                <div>
-                    <p>-16</p>
-                    <p className="maxrangeLocation nine">{+(maxrange / 1.5).toFixed(0) + 1}</p>
-                    <p>-</p>
-                    <p className="maxrangeLocation ten">{(maxrange / 1.2).toFixed(0)}</p>
-                </div>
-                <div>
-                    <p>-32</p>
-                    <p className="maxrangeLocation eleven">{+(maxrange / 1.2).toFixed(0) + 1}</p>
-                    <p>-</p>
-                    <input className="maxrangeLocation" type="number" defaultValue={maxrange} onChange={event => updateAttribute(event.target.value, "maxrange")} />
-                </div>
+            <div>
+                <p>{rangeIncrement}</p>
+                <p className="maxrangeLocation">{min}</p>
+                <p>-</p>
+                <p className="maxrangeLocation">{max}</p>
             </div>
         )
     }
@@ -96,24 +72,13 @@ export default function Ranges({ maxrange, editing, updateAttribute }) {
                 {calculateRange('-4', +(maxrange / 3).toFixed(0) + 1, +(maxrange / 2).toFixed(0))}
                 {calculateRange('-8', +(maxrange / 2).toFixed(0) + 1, +(maxrange / 1.5).toFixed(0))}
                 {calculateRange('-16', +(maxrange / 1.5).toFixed(0) + 1, +(maxrange / 1.2).toFixed(0))}
-                {calculateRange('-32', +(maxrange / 1.2).toFixed(0) + 1, maxrange)}
+                {calculateRange('-32', +(maxrange / 1.2).toFixed(0) + 1, maxrange, true)}
             </div>)
     }
 
     return (
         <div>
             {ranges}
-        </div>
-    )
-}
-
-function calculateRange(rangeIncrement, min, max) {
-    return (
-        <div>
-            <p>{rangeIncrement}</p>
-            <p className="maxrangeLocation">{min}</p>
-            <p>-</p>
-            <p className="maxrangeLocation">{max}</p>
         </div>
     )
 }
